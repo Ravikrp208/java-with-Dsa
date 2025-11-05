727bcf72bf2f3a482f4203a88d39c28470ce55d7
Ravikrp208 <ravikrp208@gmail.com>
2025-11-05 09:05:44 +0530

    first commit

---
+++ JAVA/basic/javabasic.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class javabasic
+  {
+public static void main (String args[]){
+
+  Scanner sc =new Scanner(System.in);
+  System.out.println("enter a no..");
+  int a = sc.nextInt();
+  System.out.println("enter 2nd no.. ");
+  int b = sc.nextInt(); 
+
+  int product =a*b;
+System.out.println( product);
+}
+
+}
