da6c10436925a3e0faeeb584b510c6673f658606
Ravikrp208 <ravikrp208@gmail.com>
2025-11-10 23:11:58 +0530

    patten

---
+++ JAVA DSA/Patten/printstar.java
@@ -0,0 +1,12 @@
+
+public class printstar {
+    public static void main(String[] args) {
+        for (int line=1; line<=4; line++){
+
+          for(int start=1; start<=line; start++){
+             System.out.print("*"); 
+        }
+        System.out.println();
+     }
+    }
+}
--- JAVA DSA/operater/Arithmetic operator/arithmeticjava.java
+++ JAVA DSA/operater/Arithmetic operator/arithmeticjava.java
@@ -4,8 +4,12 @@ public class arithmeticjava {
     public static void main(String[] args) {
         int A = 10;
         int B = 5;
+    
 
         System.out.println("Addition = " + (A + B));
         System.out.println("Subtraction = " + (A - B));
+     
+
+
     }
 }
