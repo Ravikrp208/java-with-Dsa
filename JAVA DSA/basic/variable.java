640aa902e5558cff3e96c70a539a0290f1b3b4a3
Ravikrp208 <ravikrp208@gmail.com>
2025-11-11 23:50:07 +0530

    basic

---
+++ JAVA DSA/basic/variable.java
@@ -0,0 +1,18 @@
+public class variable {
+    public static void main(String[] args) {
+        String studentName ="Ravi kumar pandit";
+        int studentId =25;
+        int studentAge =23;
+        float studentFee =75.25f;
+        char studentGrade = 'A';
+
+        System.out.println("Student name : "+studentName);
+        System.out.println("student id: "+ studentId);
+        System.out.println("student age: "+ studentAge);
+        System.out.println("student fee: "+ studentFee);
+        System.out.println("student grade: "+ studentGrade);
+
+    }
+    
+}
+ 
