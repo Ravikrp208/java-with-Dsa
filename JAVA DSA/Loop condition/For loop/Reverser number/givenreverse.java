cb1ea4dae76b193e0ebc087ca3c0a997f0267b08
Ravikrp208 <ravikrp208@gmail.com>
2025-11-18 06:30:48 +0530

    do loop

--- JAVA DSA/Loop condition/For loop/Reverser number/givenreverse.java
+++ JAVA DSA/Loop condition/For loop/Reverser number/givenreverse.java
@@ -1,14 +1,14 @@
 
 public class givenreverse{
     public static void main(String [] args){
-        int n = 123;
+        int n = 1320;
         int rev = 0;
 
         while (n > 0) {
             int lastno = n % 10;
             rev = (rev * 10) + lastno;  
             n = n / 10;
-           System.out.println(lastno);
+        //    System.out.print(lastno);
         }
         System.out.println(rev);
         
---
+++ JAVA DSA/Loop condition/do while loop/dowhileloop.java
@@ -0,0 +1,14 @@
+
+public class dowhileloop {
+    public static void main (String []args){
+        int i =1;
+        
+        do { 
+
+            System.out.println("Ravi kumar pandit");
+            i++;
+        }
+         while (i<=10);
+    }
+    
+}
