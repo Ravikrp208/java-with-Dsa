1da806ab4a7cafa296aafe82910d79e59a611c09
Ravikrp208 <ravikrp208@gmail.com>
2025-11-18 23:45:53 +0530

    prime no

---
+++ JAVA DSA/Loop condition/Check prime no/primeno.java
@@ -0,0 +1,38 @@
+
+import java.util.Scanner;
+
+public class primeno {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number ...");
+        int n =sc.nextInt();
+
+        if(n==2)
+        {
+            System.out.println("n is a prime number ");
+        }
+
+        else
+        {
+            boolean isprime=true;
+            for (int i=2; i<=n-1; i++)
+            {
+                if(n%i==0)
+                {
+                    isprime =false;
+                }
+            }
+
+            if(isprime==true)
+            {
+                System.out.println("n is prime");
+            }
+
+            else
+            {
+               System.out.println("n is not prime");
+            }
+         
+        }
+    }
+}
