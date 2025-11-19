15a477bca4e873ae2e1a22c0ae7f8e479536ab90
Ravikrp208 <ravikrp208@gmail.com>
2025-11-20 01:59:55 +0530

    function

---
+++ JAVA DSA/Function- Method/function.java
@@ -0,0 +1,16 @@
+
+public class function {
+    public static void printhelloRavi(){
+        System.out.println("hello Ravi kumar pandit");
+        System.out.println("i am a computer scirnce student");
+        System.out.println("i have bca in 2025");
+        System.out.println("i am pursuing MCA");
+
+        // return;
+    }
+    public static void main(String []args){
+        printhelloRavi();//function call  
+
+    }
+    
+}
