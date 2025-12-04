05bd2538ef4714225002a0aa083fff85723970ce
Ravikrp208 <ravikrp208@gmail.com>
2025-12-04 23:47:58 +0530

    shorting arry

--- JAVA DSA/shorting arry/shorting.java
+++ JAVA DSA/shorting arry/shorting.java
@@ -1,1 +1,13 @@
 
+import java.util.Arrays;
+
+public class shorting{
+    public static void main(String[] args) {
+
+        int[] arr = {5, 2, 9, 5,3,1, 7};
+
+        Arrays.sort(arr); 
+
+        System.out.println(Arrays.toString(arr));
+    }
+}
