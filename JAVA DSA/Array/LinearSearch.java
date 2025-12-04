c074420eb9dceb540471115580d2d586bea7b7f7
Ravikrp208 <ravikrp208@gmail.com>
2025-12-04 23:55:32 +0530

    Array

---
+++ JAVA DSA/Array/LinearSearch.java
@@ -0,0 +1,27 @@
+// package JAVA DSA.Array with liner search ;
+
+public class LinearSearch {
+    public static void main(String[] args) 
+    {
+
+        int[] arr = {10, 25, 30, 45, 50};
+        int target = 30;
+
+        int index = linearSearch(arr, target);
+
+        if(index != -1)
+            System.out.println("Element found at index: " + index);
+        else
+            System.out.println("Element not found.");
+    }
+
+    public static int linearSearch(int[] arr, int target) 
+    {
+        for(int i = 0; i < arr.length; i++) {
+            if(arr[i] == target) {
+                return i;  
+            }
+        }
+        return -1;  // Not found
+    }
+}
