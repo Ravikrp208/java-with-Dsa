65ced02e9ac04ccfeea714d7fdb24ff84d69d6a6
Ravikrp208 <ravikrp208@gmail.com>
2025-12-06 22:44:50 +0530

    Arry basic

---
+++ JAVA DSA/Array/Basic Array/CheckSorted.java
@@ -0,0 +1,17 @@
+public class CheckSorted {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5,8};
+
+        boolean sorted = true;
+
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] < arr[i - 1]) {
+                sorted = false;
+                break;
+            }
+        }
+
+        System.out.println(sorted ? "Sorted" : "Not sorted");
+    }
+}
+
