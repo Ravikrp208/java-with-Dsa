88b98e4052dcb8ea82d75c7d80be4d158cf10bc3
Ravikrp208 <ravikrp208@gmail.com>
2025-12-23 23:43:53 +0530

    pattern part -3

---
+++ JAVA DSA/Pattern Part -3/NumberPyramid.java
@@ -0,0 +1,22 @@
+// package JAVA DSA.Pattern Part -3;
+
+public class NumberPyramid {
+   public static void number_pyramid(int n){
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=n-i; j++){
+            System.out.print(" ");
+        }
+
+        //number print hoga
+        for(int j=1; j<=i; j++){
+            System.out.print(i +" ");
+        }
+          System.out.println();
+    }
+  
+   }
+    
+   public static void main(String[] args) {
+       number_pyramid(5);
+   }
+}
--- JAVA DSA/Pattern Part 2/diamondPattern.java
+++ JAVA DSA/Pattern Part 2/diamondPattern.java
@@ -2,19 +2,27 @@ public class diamondPattern {
     public static void Diamond_Pattern(int n){
         for(int i=1; i<=n; i++){
             for(int j=1; j<=(n-i); j++){
+
+                //space print hoga
                 System.out.print(" ");
 
             }
+
+            //start print hoga
             for(int j=1; j<=(2*i)-1; j++){
                 System.out.print("*");
             }
             System.out.println();
-        }
+         }
+         //2nd half print hoga
          for(int i=n; i>=1; i--){
             for(int j=1; j<=(n-i); j++){
+
+                //space print hoga
                 System.out.print(" ");
 
             }
+            //start print hoga
             for(int j=1; j<=(2*i)-1; j++){
                 System.out.print("*");
             }
