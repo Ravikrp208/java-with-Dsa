3f44382dc1efaa317fbcd9c229c55a9273715216
Ravikrp208 <ravikrp208@gmail.com>
2025-12-28 21:06:15 +0530

    sum of array

---
+++ JAVA DSA/Array/Basic Array/ArraySum.java
@@ -0,0 +1,14 @@
+// Array se number sum in java  
+
+public class ArraySum {
+    public static void main(String[] args) {
+        int[] arr = {2, 4, 6, 8};
+        int sum = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+        }
+
+        System.out.println("Sum = " + sum);
+    }
+}
