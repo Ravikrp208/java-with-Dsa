362617a9df8675f4c9e2455f9345c80702811f71
Ravikrp208 <ravikrp208@gmail.com>
2025-12-31 22:23:54 +0530

    arry

---
+++ JAVA DSA/Array/Basic Array/StringArra.java
@@ -0,0 +1,10 @@
+public class StringArra {
+    public static void main(String[] args) {
+
+        String[] names = {"Raj", "shivam", "manish"};
+
+        for (String name : names) {
+            System.out.println(name);
+        }
+    }
+}
