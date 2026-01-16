4f94b21bbbc708a68e1e3a8e041cd31ef4a4ae04
Ravikrp208 <ravikrp208@gmail.com>
2026-01-16 23:58:15 +0530

    Pair

--- JAVA DSA/Array/Array Part 1/Pair.java
+++ JAVA DSA/Array/Array Part 1/Pair.java
@@ -1,18 +1,18 @@
 //  how to solve pair elements  ex:- [2,4,6,8,10]
 public class Pair {
- public static void pariPairs(int num []){
-    for (int i=0; i<=num; i++){
-        int curr =num [i]; //{2,4,6,8,10};
-        for (int j=i+1; j<num.length; j++ )
-        {
-            System.out.println("(" + curr + "," + num [j] + ")");
+
+    public static void printPairs(int num[]) {
+        for (int i = 0; i < num.length; i++) {
+            int curr = num[i];
+            for (int j = i + 1; j < num.length; j++) {
+                System.out.print("(" + curr + "," + num[j] + ") ");
+            }
+            System.out.println();
         }
-     System.out.println();
     }
- }
 
     public static void main(String[] args) {
-        int num [] ={2,4,6,8,10};
+        int num[] = {2, 4, 6, 8, 10};
+        printPairs(num);   // method call
     }
-    
 }
