0af427c37f3ccff4996b351fb636726ef439ec66
Ravikrp208 <ravikrp208@gmail.com>
2026-01-17 23:50:48 +0530

    sub pair

--- JAVA DSA/Array/Array Part 1/Pair.java
+++ JAVA DSA/Array/Array Part 1/Pair.java
@@ -16,3 +16,5 @@ public class Pair {
         printPairs(num);   // method call
     }
 }
+
+
---
+++ JAVA DSA/Array/Array Part 1/Pairsub.java
@@ -0,0 +1,18 @@
+//print subarrays //
+// a continuous part of array //
+import java.util.*;
+public class Pairsub{
+    public satic void prinsub (int numbers[]){
+        for (int i=0; i<numbers.length; i++){
+            int start = i;
+            for( int j=i; j<numbers.length; j++){
+                int end = j;
+            }
+
+        }
+    }
+    public static void main(String[] args) {
+        int numbers[] ={2,4,6,8,10}
+    }
+    
+}
