00acbd5fe5f145f6cccdd6f52801e69151c83993
Ravikrp208 <ravikrp208@gmail.com>
2026-02-07 23:57:10 +0530

    two array

--- JAVA DSA/Array/2D  Arry in java/two_Matrix.java
+++ JAVA DSA/Array/2D  Arry in java/two_Matrix.java
@@ -1,19 +1,27 @@
-
 import java.util.Scanner;
 
-public class two_Matrix{
-     public static void main(String[] args) {
-        int matrix [] [] =new int [3] [3];
-        int n = matrix.length, m = matrix [0].length;
+public class two_Matrix {
+    public static void main(String[] args) {
 
-        Scanner sc =new Scanner(System.in);
-        for(int i=0; i<n; i++)
-        {
-            for(int j=0; j<n; j++)
-            {
-                matrix[i] [j] = sc.next();
+        int matrix[][] = new int[3][3];
+        int n = matrix.length;
+        int m = matrix[0].length;
+
+        Scanner sc = new Scanner(System.in);
+
+        // Input
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                matrix[i][j] = sc.nextInt();
             }
         }
-    }
 
+        // Output
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
 }
