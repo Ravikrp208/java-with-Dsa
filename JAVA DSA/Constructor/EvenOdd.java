496c84324be95e8ff9f53e4d1e5c792f83279030
Ravikrp208 <ravikrp208@gmail.com>
2026-04-27 23:30:54 +0530

    Even no check out

---
+++ JAVA DSA/Constructor/EvenOdd.java
@@ -0,0 +1,12 @@
+// 4. Even or Odd Number Check
+class EvenOdd {
+    public static void main(String[] args) {
+        int num = 7;
+
+        if(num % 2 == 0) {
+            System.out.println(num + " is Even Number");
+        } else {
+            System.out.println(num + " is Odd Number");
+        }
+    }
+}
