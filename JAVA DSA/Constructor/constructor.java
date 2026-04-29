27ed02bec0e74e5200e516181acfdfa58b7d56e1
Ravikrp208 <ravikrp208@gmail.com>
2026-04-29 23:50:24 +0530

    constructor

---
+++ JAVA DSA/Constructor/constructor.java
@@ -0,0 +1,13 @@
+// 1. Default Constructor
+class constructor {
+    String name;
+
+    constructor() {
+        name = "Ravi";
+    }
+
+    public static void main(String[] args) {
+        constructor c = new constructor();
+        System.out.println("Name: " + c.name);
+    }
+}
