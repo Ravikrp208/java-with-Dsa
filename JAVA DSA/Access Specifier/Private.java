aff8ab1ef8f037d59e7a9b5f4d81b3fcd87e66ea
Ravikrp208 <ravikrp208@gmail.com>
2026-05-18 23:17:56 +0530

    Pravite

--- JAVA DSA/Access Specifier/Private.java
+++ JAVA DSA/Access Specifier/Private.java
@@ -1,15 +1,19 @@
 // package JAVA DSA.Access Specifier;
-
 class Test  {
 
-    public int a = 10;
+    private int a = 20;
+
+    private void show() {
+        System.out.println("Private Method");
+    }
 
     public void display() {
         System.out.println("Value of a: " + a);
+        show();
     }
 }
 
-public class Main {
+public class Private {
 
     public static void main(String[] args) {
 
