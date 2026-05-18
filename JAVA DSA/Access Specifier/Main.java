3fd57d0fb748f84d591d638b23c1224f81f71c57
Ravikrp208 <ravikrp208@gmail.com>
2026-05-18 23:09:10 +0530

    Access specifier modifier

---
+++ JAVA DSA/Access Specifier/Main.java
@@ -0,0 +1,20 @@
+// package JAVA DSA.Access Specifier;
+
+class Test  {
+
+    public int a = 10;
+
+    public void display() {
+        System.out.println("Value of a: " + a);
+    }
+}
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Test t = new Test();
+
+        t.display();
+    }
+}
