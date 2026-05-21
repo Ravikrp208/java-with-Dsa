5521bd138be8b1003a015ce375ff933165b6d243
Ravikrp208 <ravikrp208@gmail.com>
2026-05-21 22:41:34 +0530

    Package in java

---
+++ JAVA DSA/Package/javasimple/A.java
@@ -0,0 +1,11 @@
+// package JAVA DSA.Package.javasimple;
+
+package javasimple;
+
+public class A
+{
+    public void displayA()
+    {
+        System.out.println("This is method of package A");
+    }
+}
---
+++ JAVA DSA/Package/simple.java
@@ -0,0 +1,11 @@
+import javasimple.A;
+
+public class simple
+{
+    public static void main(String[] args)
+    {
+        A a = new A();
+
+        a.displayA();
+    }
+}
