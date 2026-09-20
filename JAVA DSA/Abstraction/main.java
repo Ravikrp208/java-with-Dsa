3e0e3aa295ef456ff25c00ee6a20ac2eccc6d53b
Ravikrp208 <ravikrp208@gmail.com>
2026-09-20 23:22:45 +0530

    update the java code

--- JAVA DSA/Abstraction/main.java
+++ JAVA DSA/Abstraction/main.java
@@ -1,20 +1,28 @@
+// Java program to demonstrate the concept of abstraction   
 
-
-abstract class animal {
+abstract class animal 
+{
     abstract void sound();
 
-    void eat() {
+    void eat()
+     {
         System.out.println("Animal is eating");
     }
 
 }
-class dog extends animal {
-    void sound() {
+
+class dog extends animal 
+{
+    void sound() 
+    {
         System.out.println("Dog barks");
     }
 }
-public class main {
-    public static void main(String[] args) {
+
+public class main
+ {
+    public static void main(String[] args) 
+    {
         dog d = new dog();
         d.sound();
         d.eat();
