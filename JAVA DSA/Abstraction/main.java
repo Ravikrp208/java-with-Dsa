eef5b0af53f21723762becabdb48f6b09160325a
Ravikrp208 <ravikrp208@gmail.com>
2026-09-20 23:20:21 +0530

    Abstraction in java

---
+++ JAVA DSA/Abstraction/main.java
@@ -0,0 +1,22 @@
+
+
+abstract class animal {
+    abstract void sound();
+
+    void eat() {
+        System.out.println("Animal is eating");
+    }
+
+}
+class dog extends animal {
+    void sound() {
+        System.out.println("Dog barks");
+    }
+}
+public class main {
+    public static void main(String[] args) {
+        dog d = new dog();
+        d.sound();
+        d.eat();
+    }
+}
